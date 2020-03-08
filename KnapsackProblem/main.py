c404f71a276e5fb8e9e072f1a4167586df204fd6
Patryk Szwed <patryk.szwed@outlook.com>
2020-03-09 00:12:37 +0100

    Fix for plots

--- KnapsackProblem/main.py
+++ KnapsackProblem/main.py
@@ -1,5 +1,6 @@
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 from Population import Population
 from generate import generate
@@ -15,7 +16,7 @@ def get_good_solution(task,
                       crossover_rate=CROSSOVER_RATE, mutation_rate=MUTATION_RATE,
                       tournament_size=TOURNAMENT_SIZE, population_size=POPULATION_SIZE):
     best_individuals_values = []
-    generation_indexes = range(MAX_ITERATIONS)
+
     population = init_population(task, population_size)
     print('Initial value of the knapsack =', population.individuals[0].evaluate())
     outer_iterator = 0
@@ -37,21 +38,33 @@ def get_good_solution(task,
         # print('len(new_population.individuals) =', len(new_population.individuals))
         # print('population.best_individual() = ', population.best_individual())
         best_individuals_values.append(population.best_individual().evaluate())
-    my_label = 'crossover_rate = ', crossover_rate
-    plt.plot(generation_indexes, best_individuals_values, label=my_label)
-    return population.best_individual()
+    return population.best_individual(), best_individuals_values
 
 
 def main():
     generate(NUMBER_OF_OBJECTS, MAXIMUM_WEIGHT, MAXIMUM_SIZE, FILE_NAME)
     task = read(FILE_NAME)
+    generation_indexes = np.asarray(range(MAX_ITERATIONS))
 
-    for i in range(1):
+    for i in range(3):
         crossover_rate = CROSSOVER_RATES[i]
         start_time = time.time()
-        good_solution = get_good_solution(task, crossover_rate)
-        print('Final value of the knapsack =', good_solution.evaluate())
-        print('Execution time =', round(time.time() - start_time, 2), 'seconds')
+        all_solutions = []
+        for j in range(5):
+            sol = get_good_solution(task, crossover_rate)[1]
+            print('sol = ', sol)
+            print('np.asarray(sol) = ', np.asarray(sol))
+            all_solutions.append(np.asarray(sol))
+            # print('Final value of the knapsack =', good_solution.evaluate())
+            # print('Execution time =', round(time.time() - start_time, 2), 'seconds')
+        all_solutions = np.asarray(all_solutions)
+        summed_solution = all_solutions[0] + all_solutions[1] + all_solutions[2] + all_solutions[3] + all_solutions[4]
+        print('summed_solution = ', summed_solution)
+        average_solution = np.true_divide(summed_solution, 5)
+        my_label = 'crossover_rate = ', crossover_rate
+        print('type(generation_indexes) = ', type(generation_indexes))
+        print('type(average_solution) = ', type(average_solution))
+        plt.plot(generation_indexes, average_solution, label=my_label)
     plt.xlabel('Generation')
     plt.ylabel('Knapsack\'s value')
     plt.legend()
