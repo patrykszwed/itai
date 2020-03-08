6511980a39efad3254d0643e43f7f5057b16cbf0
Patryk Szwed <patryk.szwed@outlook.com>
2020-03-08 14:51:35 +0100

    Bugfix for the tournament.py get_best_individual()

--- KnapsackProblem/tournament.py
+++ KnapsackProblem/tournament.py
@@ -3,12 +3,14 @@ from helpers import get_random_indexes_from_array
 
 def get_best_individual(population_individuals, random_indexes):
     best_individual_value = 0
-    best_individual_index = -1
+    best_individual = population_individuals[0]
     for index, individual_index in enumerate(random_indexes):
-        individual_value = population_individuals[individual_index].evaluate()
+        individual = population_individuals[individual_index]
+        individual_value = individual.evaluate()
         if individual_value > best_individual_value:
-            best_individual_index = individual_index
-    return population_individuals[best_individual_index]
+            best_individual_value = individual_value
+            best_individual = individual
+    return best_individual
 
 
 def tournament(population, tournament_size):
